861acd138536513e5ae0a8a2ffbc5cb168044dad
Fabien Chiffre <fabien.chiffre@gmail.com>
2016-05-09 10:53:01 +0200

    Add Ingredient class...

---
+++ src/Model/Ingrediente.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Ingrediente {
+
+	private String nome;
+
+	public Ingrediente(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+}
